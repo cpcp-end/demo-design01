825bbf1d82448c8dd934943cccd5f31b122859ae
本田勇 <hond@hondaisamunoMacBook-Pro.local>
2021-01-31 05:41:35 +0900

    demo-design01

---
+++ js/ScrollReveal.js
@@ -0,0 +1,85 @@
+// banner-content-imgbox
+
+ScrollReveal().reveal('.banner-content-imgbox-left', { 
+    duration: 4000,
+    origin: 'left',
+    distance: '100px',
+    reset: false   
+  });
+
+ScrollReveal().reveal('.banner-content-imgbox-right', { 
+    duration: 4000,
+    origin: 'right',
+    distance: '100px',
+    reset: false   
+  });
+
+//  about
+
+ScrollReveal().reveal('.about-img', { 
+    duration: 4000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.about-img1', { 
+    duration: 4000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.about-img2', { 
+    duration: 4000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.about-img3', { 
+    duration: 4000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.about-img4', { 
+    duration: 4000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.about-img5', { 
+    duration: 4000,
+    reset: false   
+  });
+
+//   sweets-commitmentbox
+
+ScrollReveal().reveal('.sweets-commitment-img', { 
+    delay: 100,
+    duration: 2000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.sweets-commitment-img1', { 
+    delay: 200,
+    duration: 2000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.sweets-commitment-img2', { 
+    delay: 200,
+    duration: 2000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.sweets-commitment-img3', { 
+    delay: 400,
+    duration: 2000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.sweets-commitment-img4', { 
+    delay: 500,
+    duration: 2000,
+    reset: false   
+  });
+
+ScrollReveal().reveal('.sweets-commitment-img5', { 
+    delay: 600,
+    duration: 2000,
+    reset: false   
+  });
---
+++ js/jQuery.js
@@ -0,0 +1,9 @@
+
+// top-wrapper-imgbox
+
+$(function () {
+    $('.gnav-js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+      $('.top-menu , .gnav-btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+    })
+  });
+
